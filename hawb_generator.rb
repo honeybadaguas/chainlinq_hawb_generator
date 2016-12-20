ef0e97e57640cbe59ecf3b6c63a9cd9e99325512
Hannah Badaguas <hannah.badaguas@gmail.com>
2016-12-20 08:49:08 +0800

    added skipping of CL checking

--- hawb_generator.rb
+++ hawb_generator.rb
@@ -3,15 +3,16 @@ require_relative 'chainlinq_connector'
 
 class HawbGenerator
   class << self
-    def generate_hawb(file_path, prefix, count, num_count=11)
+    def generate_hawb(file_path, prefix, count, num_count=11, check_chainlinq=true)
       file = File.new(file_path, "w")
       i = 0
       loop do
         num = Luhn.generate(num_count)
         hawb = "#{prefix}#{num}"
-        unless ChainlinqConnector.hawb_exists?(hawb)
+        if (!check_chainlinq || !ChainlinqConnector.hawb_exists?(hawb))
           file.write("#{hawb}\n")
           i += 1
+          puts i
         end
         break if i == count
       end
